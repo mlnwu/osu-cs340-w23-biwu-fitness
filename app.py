5a08552c6e6ebdf375a2745d17e3083d2a955fcb
Mia Bilka <bilkam@oregonstate.edu>
2023-03-01 23:15:29 -0800

    Reworked existing HTML pages to work as a Flask web app

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect, url_for
+import os
+
+# Config
+
+app = Flask(__name__)
+
+# Routes
+
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/')
+def root():
+    return redirect(url_for('index'))
+
+@app.route('/classes')
+def classes():
+    return render_template("classes.html")
+
+@app.route('/members')
+def members():
+    return render_template("members.html")
+
+@app.route('/trainers')
+def trainers():
+    return render_template("trainers.html")
+
+@app.route('/transactions')
+def transactions():
+    return render_template("transactions.html")
+
+# Listener
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 29901))
+    app.run(port=port)
